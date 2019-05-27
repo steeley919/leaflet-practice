ce65c27578e3d5f4a1a8c8582a13628fbdfbab60
Molly Carlson <molly.carlson1120@gmail.com>
2019-05-26 22:47:02 -0400

    finished tutorial

--- review.js
+++ review.js
@@ -5,6 +5,8 @@
 **/
 var L = window.L;
 document.onload = loadMap();
+var popup = L.popUp();
+
 
 function loadMap() {
     var starterMap = L.map('map').setView([51.505, -0.09], 13);
@@ -16,7 +18,52 @@ function loadMap() {
         accessToken: 'pk.eyJ1IjoiY2FybHNvbSIsImEiOiJjanc1cHhmankxMXl2NGFvd2p5dDJxYjUyIn0.c3Ky0uPnrXYgHbHM72EZ3w'
     }).addTo(starterMap);
 
+    //make this stand out a little more
+
+    var circle = L.circle([51.508, -0.11], {
+        color:'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(starterMap);
+
+    var polygon = L.polygon([
+        [51.509, -0.078],
+        [51.503, -0.06],
+        [51.51, -0.047]
+    ], {
+        color: '#d99645',
+        fillColor:'#ffffff',
+        fillOpacity: 0.5
+    }).addTo(starterMap);
+
+    /* Markers and Popups*/
+    var marker = L.marker([51.505, -0.09]).addTo(starterMap);
+
+    marker.bindPopup("<strong> Popup representing this segment of the United Kingdom!</strong>").openPopup();
+    circle.bindPopup("Circle");
+
+    /* Using Popups as Layers */
+    var standAlonePopup = L.popup()
+        .setLatLng([51.512, -0.09])
+        .setContent("Standalone Popup here!")
+        .openOn(starterMap);
+
+    starterMap.on('click', onMapClick);
+
 }
+
+/* Last section: Events */
+var popup2 = L.popUp();
+
+function onMapClick(e) {
+ popup2
+     .setLatLng(e.latlng)
+     .setContent("You just clicked the map at:" + e.latlng.toString())
+     .openOn(starterMap);
+}
+
+
 
 
 
